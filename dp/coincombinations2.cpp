c792cf00e86ccbd64bc880dbc90306f9adce1999
andrew <asc269@cornell.edu>
2023-01-25 18:02:53 -0500

    Coin Combinations II

---
+++ dp/coincombinations2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int mod = 1e9+7;
+  int n, target;
+  cin >> n >> target;
+
+  vector<int> c(n);
+  for (int&v : c) {
+    cin >> v;
+  }
+
+  vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+  dp[0][0] = 1;
+  for(int i = 1; i <= n; i++) {
+    for(int j = 0; j <= target; j++) {
+      dp[i][j] = dp[i - 1][j];
+      int left = j - c[i - 1];
+      if(left >= 0) {
+        (dp[i][j] += dp[i][left]) %= mod;
+      }
+
+    }
+  }
+  cout << dp[n][target] << endl;
+}
