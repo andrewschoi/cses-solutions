b23f7e8cac7a2906e27db5887950b5508ca27a88
andrew <asc269@cornell.edu>
2023-06-15 09:12:52 -0500

    minimizing coins

--- dp/minimizingcoins.cpp
+++ dp/minimizingcoins.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int n, target;
-	cin >> n >> target;
-	vector<int> c(n);
-	for (int&v : c) cin >> v;
-
-	vector<int> dp(target + 1, 1e9);
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	vector<long> coins(n);
+	for (int i = 0; i < n; i++)
+		cin >> coins[i];
+	vector<long> dp(x + 1, 1e7);
 	dp[0] = 0;
-	for (int i = 0; i <= target; i++) {
-		for (int j = 0; j < n; j++) {
-			if (i - c[j] >= 0) {
-				dp[i] = min(dp[i], dp[i - c[j]] + 1);
+	for (long &coin : coins)
+	{
+		if (coin <= x)
+			dp[coin] = 1;
+	}
+
+	for (int i = 0; i <= x; i++)
+	{
+		for (long &coin : coins)
+		{
+			if (i - coin >= 0)
+			{
+				dp[i] = min(dp[i], 1 + dp[i - coin]);
 			}
 		}
- 	}
-
-	cout << (dp[target] == 1e9 ? -1 : dp[target]) << endl;
+	}
+	if (dp[x] != 1e7)
+		cout << dp[x] << "\n";
+	else
+		cout << -1 << "\n";
 }
