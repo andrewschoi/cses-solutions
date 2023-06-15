af9d435bcb0a61fdfa424ccdcd620a716bc67d85
andrew <asc269@cornell.edu>
2023-06-15 09:12:09 -0500

    dice combinations

--- dp/dicecombinations.cpp
+++ dp/dicecombinations.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
   int mod = 1e9 + 7;
   int n;
   cin >> n;
-  vector<int> dp(n + 1, 0);
+  vector<long long> dp(n + 1, 0);
   dp[0] = 1;
 
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= 6 && i - j >= 0; j ++) {
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= 6 && i - j >= 0; j++)
+    {
       (dp[i] += dp[i - j]) %= mod;
     }
   }
 
-  cout << dp[n] << endl;
+  cout << dp[n] << "\n";
 }
