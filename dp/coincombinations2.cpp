5bb5a88494f22902e022fcbb7227c5ba9a48ccc8
andrew <asc269@cornell.edu>
2023-06-15 09:11:48 -0500

    coin combinations 2

--- dp/coincombinations2.cpp
+++ dp/coincombinations2.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int mod = 1e9+7;
-  int n, target;
-  cin >> n >> target;
-
-  vector<int> c(n);
-  for (int&v : c) {
-    cin >> v;
-  }
-
-  vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+int main()
+{
+  int mod = 1e9 + 7;
+  int n, x;
+  cin >> n >> x;
+  vector<int> coins(n);
+  for (int &coin : coins)
+    cin >> coin;
+  vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
   dp[0][0] = 1;
-  for(int i = 1; i <= n; i++) {
-    for(int j = 0; j <= target; j++) {
-      dp[i][j] = dp[i - 1][j];
-      int left = j - c[i - 1];
-      if(left >= 0) {
-        (dp[i][j] += dp[i][left]) %= mod;
-      }
 
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 0; j <= x; j++)
+    {
+      dp[i][j] = dp[i - 1][j];
+      if (j - coins[i - 1] >= 0)
+      {
+        (dp[i][j] += dp[i][j - coins[i - 1]]) %= mod;
+      }
     }
   }
-  cout << dp[n][target] << endl;
+
+  cout << dp[n][x] << "\n";
 }
