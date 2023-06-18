bdca5aae221a3a061dd19f8c4e3a24d001850532
andrew <asc269@cornell.edu>
2023-06-18 12:43:56 -0500

    counting rooms

--- graph/coutningrooms.cpp
+++ graph/coutningrooms.cpp
@@ -1,62 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int neighborX[4] = {0,0,-1,1};
-int neighborY[4] = {-1,1,0,0};
+int neighborX[4] = {0, 0, -1, 1};
+int neighborY[4] = {1, -1, 0, 0};
 
-int n, m, ans = 0;
-int vis[1001][1001];
-char grid[1001][1001];
+int n, m, answer;
+int vis[1010][1010];
+char grid[1010][1010];
 
-bool isValid(int y, int x) {
-  if (y < 0) {
+bool isValid(int y, int x)
+{
+  if (y < 0)
     return false;
-  }
-  if (x < 0) {
+  if (x < 0)
     return false;
-  }
-  if (y >= n) {
+  if (y >= n)
     return false;
-  }
-  if (x >= m) {
+  if (x >= m)
     return false;
-  }
-  if (grid[y][x] == '#') {
+  if (grid[y][x] == '#')
     return false;
-  }
   return true;
 }
 
-void dfs(int y, int x) {
-  vis[y][x] = 1;
-  for(int i = 0; i < 4; i++) {
-    int newX = neighborX[i] + x;
-    int newY = neighborY[i] + y;
-    if (isValid(newY, newX)) {
-      if(!vis[newY][newX]) {
-        dfs(newY, newX);
+void DFS(int x, int y)
+{
+  vis[x][y] = 1;
+  for (int i = 0; i < 4; i++)
+  {
+    int newX = x + neighborX[i];
+    int newY = y + neighborY[i];
+    if (isValid(newX, newY))
+    {
+      if (!vis[newX][newY])
+      {
+        DFS(newX, newY);
       }
     }
   }
 }
 
-int main() {
+int main()
+{
   cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
       cin >> grid[i][j];
       vis[i][j] = 0;
     }
   }
-
-  for (int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      if(grid[i][j] == '.' && !vis[i][j]) {
-        dfs(i, j);
-        ans++;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (grid[i][j] == '.' && !vis[i][j])
+      {
+        DFS(i, j);
+        answer++;
       }
     }
   }
-
-  cout << ans << endl;
+  cout << answer;
 }
