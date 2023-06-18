d6f924d4c788ac6a44ea6d7057c69a4799129b1a
andrew <asc269@cornell.edu>
2023-06-18 12:42:02 -0500

    book shops

--- dp/bookshop.cpp
+++ dp/bookshop.cpp
@@ -1,23 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
   int n, x;
   cin >> n >> x;
-  vector<int> prices(n), pages(n);
-  for(int&v : prices) cin >> v;
-  for(int&v : pages) cin >> v;
-  
+  vector<int> prices(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> prices[i];
+  }
+  vector<int> pages(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> pages[i];
+  }
+
   vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
-  for(int i = 1; i <= n; i++) {
-    for(int j = 0; j <= x; j++) {
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 0; j <= x; j++)
+    {
       dp[i][j] = dp[i - 1][j];
-      int left = j - prices[i - 1];
-      if (left >= 0) {
-        dp[i][j] = max(dp[i][j], dp[i - 1][left] + pages[i - 1]);
+      if (j - prices[i - 1] >= 0)
+      {
+        dp[i][j] = max(dp[i][j], dp[i - 1][j - prices[i - 1]] + pages[i - 1]);
       }
     }
   }
 
-  cout << dp[n][x] << endl;
+  cout << dp[n][x];
 }
